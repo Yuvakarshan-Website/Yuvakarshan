02892dfa45a9c314ec5231a9d7d1515c8884bc31
xxxGitHubxx <ridgosh@gmail.com>
2025-05-22 00:31:17 +0530

    rid test commit

--- public/pages/home.js
+++ public/pages/home.js
@@ -2,8 +2,12 @@ export default function Home() {
   const container = document.createElement('div');
   container.className = 'home';
 
-  container.innerHTML = `HELLOoooooooooo :DDDDDDDDDD
+  container.innerHTML = `
+      <div class="home-background">
+      <h1 class="page-title">home</h1>
+      </div>
   `;
 
   return container;
 }
+
