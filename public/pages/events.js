d20ac5870da4cdc8728f33e5782483121258f0ca
Aviral Kochgaway <aviral.kochgaway@gmail.com>
2025-05-21 22:37:25 +0530

    events page part 1

--- public/pages/events.js
+++ public/pages/events.js
@@ -4,12 +4,37 @@ export default function Events() {
 
   container.innerHTML = `
     <section class="events-list">
-      <h1 class="page-title">Events</h1>
-      <ul>
-        <li>Hackathon 2025</li>
-        <li>Science Expo</li>
-        <li>Literary Debate is gay</li>
-      </ul>
+        <div class="card-container">
+          <div class="card-grid ">
+            <!-- Repeat this block for each card -->
+            <div class="card">
+              <img src="https://media.istockphoto.com/id/1345681613/vector/creative-people-logo-vector-illustration-design-editable-resizable-eps-10.jpg?s=612x612&w=0&k=20&c=9XUHICA1ljbxBcLw8ERp0kDDxLNQ8Bp2yR4aUSS6SBs=" alt="Moot Court">
+              <div class="card-title">MOOT COURT</div>
+            </div>
+
+             <div class="card">
+              <img src="../assets/moot-icon.png" alt="Moot Court">
+              <div class="card-title">MOOT COURT</div>
+            </div>
+
+             <div class="card">
+              <img src="../assets/moot-icon.png" alt="Moot Court">
+              <div class="card-title">MOOT COURT</div>
+            </div>
+
+
+             <div class="card">
+              <img src="../assets/moot-icon.png" alt="Moot Court">
+              <div class="card-title">MOOT COURT</div>
+            </div>
+
+             <div class="card">
+              <img src="../assets/moot-icon.png" alt="Moot Court">
+              <div class="card-title">MOOT COURT</div>
+            </div>
+          </div>
+        </div>
+
     </section>
   `;
 
