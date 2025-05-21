9a9c62f48910c9b19fbfa8f376facf33990b2279
Aviral Kochgaway <aviral.kochgaway@gmail.com>
2025-05-22 01:00:20 +0530

    final

--- public/pages/events.js
+++ public/pages/events.js
@@ -9,28 +9,33 @@ export default function Events() {
             <!-- Repeat this block for each card -->
               <div class="card-row">
                 <div class="card">
-                  <img src="../assets/images/logo.jpeg" alt="Moot Court">
+                  <img src="../assets/images/logo.png" alt="Moot Court">
                   <div class="card-title">MOOT COURT</div>
                 </div>
 
                 <div class="card">
-                  <img src="../assets/moot-icon.png" alt="Moot Court">
+                  <img src="../assets/images/logo.png" alt="Moot Court">
                   <div class="card-title">MOOT COURT</div>
                 </div>
 
                 <div class="card">
-                  <img src="../assets/moot-icon.png" alt="Moot Court">
+                  <img src="../assets/images/logo.png" alt="Moot Court">
                   <div class="card-title">MOOT COURT</div>
                 </div>
 
 
                 <div class="card">
-                  <img src="../assets/moot-icon.png" alt="Moot Court">
+                  <img src="../assets/images/logo.png" alt="Moot Court">
                   <div class="card-title">MOOT COURT</div>
                 </div>
 
                 <div class="card">
-                  <img src="../assets/moot-icon.png" alt="Moot Court">
+                  <img src="../assets/images/logo.png" alt="Moot Court">
+                  <div class="card-title">MOOT COURT</div>
+                </div>
+
+                <div class="card">
+                  <img src="../assets/images/logo.png" alt="Moot Court">
                   <div class="card-title">MOOT COURT</div>
                 </div>
               </div>
