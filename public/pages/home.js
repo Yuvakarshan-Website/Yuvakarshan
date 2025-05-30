ccc829b904319522610e972a159e62c2845fd2f0
xxxGitHubxx <ridgosh@gmail.com>
2025-05-31 02:33:59 +0530

    made home 100% responsive (smooth ah)

--- public/pages/home.js
+++ public/pages/home.js
@@ -14,28 +14,26 @@ export default function Home() {
       <div class="YuvaHist Title">
         From the Yuvakarshan Directors' Desk
       </div>
-      <div class="YuvaHist">
-        <p> Derived from the Sanskrit word “Yuvakarshan,” meaning “the magnetic pull of the youth,” our fest stands as a celebration of youthful energy, intellect, artistry, and unyielding spirit. What distinguishes Yuvakarshan 2025 is not just its scale or spirit but the soul of its theme, Indian Mythology. </p>
-      </div>
-      <div class="YuvaHist">  
-        <p> This year, we immerse our fest in the timeless wisdom, valor, and mystique of ancient Indian mythology not as a nod to the past, but as a bridge to the future. The epics and legends that shaped civilizations, forged moral codes, and inspired generations now take center stage, reminding us of the power of stories rooted in culture, values, and spiritual depth. </p>
-      </div>
-      <div class="YuvaHist">
-        <p> In an age driven by algorithms and ambition, revisiting these narratives is more than homage it is a reclamation of identity. It is our tribute to the richness of Indian heritage, a celebration of symbolism, archetypes, and cosmic battles that still echo in our collective consciousness. It is through these stories that we hope to awaken in every participant a deeper understanding of leadership, strategy, art, justice, and most of all purpose. </p>
-      </div>
-      <div class="YuvaHist">
+      <p class="YuvaHist">
+         Derived from the Sanskrit word “Yuvakarshan,” meaning “the magnetic pull of the youth,” our fest stands as a celebration of youthful energy, intellect, artistry, and unyielding spirit. What distinguishes Yuvakarshan 2K25 is not just its scale or spirit but the soul of its theme, Indian Mythology. 
+      </p>
+      <p class="YuvaHist">  
+         This year, we immerse our fest in the timeless wisdom, valor, and mystique of ancient Indian mythology not as a nod to the past, but as a bridge to the future. The epics and legends that shaped civilizations, forged moral codes, and inspired generations now take center stage, reminding us of the power of stories rooted in culture, values, and spiritual depth. 
+      </p>
+      <p class="YuvaHist">
+        In an age driven by algorithms and ambition, revisiting these narratives is more than homage it is a reclamation of identity. It is our tribute to the richness of Indian heritage, a celebration of symbolism, archetypes, and cosmic battles that still echo in our collective consciousness. It is through these stories that we hope to awaken in every participant a deeper understanding of leadership, strategy, art, justice, and most of all purpose. 
+      </p>
+      <p class="YuvaHist">
         But Yuvakarshan is not one-dimensional. It is a vibrant tapestry woven from diverse threads of human potential. To ensure that every domain of creativity and skill receives its rightful spotlight, we have curated the fest into six distinct and thoughtfully structured segments
-          <br><br>
-          <div class="segments"> 
-            1.	Kartekya’s Combat <br>
-            2.	Chanakaya’s council <br>
-            3.	Kalpana <br>
-            4.	Shastra Byte <br>
-            5.	Karigari Hub <br>
-            6.	Vikram Vision<br><br> 
-          </div>
+          <br><br> 
+          &emsp;  ✧	Kartekya’s Combat <br>
+          &emsp;  ✧	Chanakaya’s Council <br>
+          &emsp;  ✧	Kalpana <br>
+          &emsp;  ✧	Shastra Byte <br>
+          &emsp;  ✧	Karigari Hub <br>
+          &emsp;  ✧	Vikram's Vision<br><br>
         Each segment is rooted in mythological essence and yet designed to challenge and captivate the contemporary mind.
-      </div>
+      </p>
       <div class="YuvaHist">
       Yuvakarshan is not just a fest it is an experience. A confluence of intellect and imagination. A platform where legends are not just remembered, but reimagined. And in this convergence, we invite schools from across the country to participate, to create, and to carve their own legacy in the grand tapestry of Yuvakarshan on the 24th and 25th of July.
       </div>
