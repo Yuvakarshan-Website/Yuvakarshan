4e3121301f1653d71157922fb0931b867394ad88
Aviral Kochgaway <aviral.kochgaway@gmail.com>
2025-07-09 18:05:09 +0530

    change of email id

--- public/pages/home.js
+++ public/pages/home.js
@@ -82,7 +82,7 @@ export default function Home() {
         </div>
 
         
-        <a href="mailto:dpsnyuvakarshan@gmail.com" aria-label="Email" class="footer-icon email">
+        <a href="mailto:yuvakarshan@dpsn.org.in" aria-label="Email" class="footer-icon email">
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="24" height="24">
             <path d="M20 4H4a2 2 0 00-2 2v12a2 2 0 002 2h16a2 2 0 002-2V6a2 2 0 00-2-2zm0 2v.01L12 13 4 6.01V6h16zM4 18V8l8 5 8-5v10H4z"/>
           </svg>
